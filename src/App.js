0d8bee84842513f55216e60b50cf56dde8f1f3a6
BianchoS <89320566+BianchoS@users.noreply.github.com>
2022-02-13 14:32:47 +0100

    Added links to the footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import './App.css';
+import "./App.css";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        Hello
-        </header>
+        <header className="App-header">Hello</header>
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer"> Coded by Bianca Slavov</footer>
-        </div>
+        <footer className="App-footer">
+          {" "}
+          <a
+            href="https://github.com/Bianca-Slavov/dictionary-project-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/biancaslavov/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Bianca Slavov
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
